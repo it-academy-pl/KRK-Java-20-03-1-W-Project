84161e712ad156669c0114edebc6ccfa0fb5bd27
anetaradek <aneta.j.radek@gmail.com>
2020-06-12 20:45:28 +0200

    override methods in GameInMemoryRepository

--- src/main/java/pl/itacademy/tictactoe/api/GameInMemoryRepository.java
+++ src/main/java/pl/itacademy/tictactoe/api/GameInMemoryRepository.java
@@ -2,6 +2,7 @@ package pl.itacademy.tictactoe.api;
 
 import pl.itacademy.tictactoe.domain.Game;
 
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
@@ -11,16 +12,30 @@ public class GameInMemoryRepository implements GameRepository {
 
     @Override
     public Game addGame(Game game) {
-        return null;
+        if (games.isEmpty()) {
+            game.setId(1);
+        } else {
+            int maxKey = games.keySet().stream().max(Comparator.naturalOrder()).get();
+            game.setId(++maxKey);
+        }
+        games.put(game.getId(), game);
+        return game;
     }
 
     @Override
     public Optional<Game> getGameById(Integer id) {
-        return Optional.empty();
+        if (!games.containsKey(id)) {
+            return Optional.empty();
+        }
+        return Optional.of(games.get(id));
     }
 
     @Override
     public Game updateGame(Game game) {
-        return null;
+        if (game.getId() == 0) {
+            //TODO: throw exception
+        }
+        games.put(game.getId(), game);
+        return game;
     }
 }
