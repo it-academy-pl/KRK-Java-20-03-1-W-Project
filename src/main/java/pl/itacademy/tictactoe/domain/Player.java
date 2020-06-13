049a30f6a270e3bff9d5e8cf59fe989e56cf2a17
olegmiercoles <oleg.miercoles@dot.com>
2020-06-13 10:30:16 +0200

    remove lombok

--- src/main/java/pl/itacademy/tictactoe/domain/Player.java
+++ src/main/java/pl/itacademy/tictactoe/domain/Player.java
@@ -1,10 +1,51 @@
 package pl.itacademy.tictactoe.domain;
 
-import lombok.Data;
+import java.util.Objects;
 
-
-@Data
 public class Player {
     private String name;
     private String password;
+
+    public Player(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Player player = (Player) o;
+        return Objects.equals(name, player.name) &&
+                Objects.equals(password, player.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, password);
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
