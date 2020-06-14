6db141e10719e97b6b7b7a4f1b5595b2ad4e8571
artek-dz <artek.dz3007@gmail.com>
2020-06-14 22:03:35 +0200

    - extracted private methods and refactor

--- src/main/java/pl/itacademy/tictactoe/api/GameService.java
+++ src/main/java/pl/itacademy/tictactoe/api/GameService.java
@@ -39,88 +39,86 @@ public class GameService implements GameInterface {
         Game game = repository.getGameById(move.getGameId())
                 .orElseThrow(() -> new GameNotFoundException("Game [" + move.getGameId() + "] not found"));
 
-        char cellValue = game.getBoard()[move.getCellIndex()];
+        checkCellIndex(move.getCellIndex(), game);
+
+        checkPlayer(move.getPlayer(), game);
+
+        char moveChar = getMoveChar(move.getPlayer(), game);
+        checkMove(moveChar, game);
+
+        game.getBoard()[move.getCellIndex()] = moveChar;
+        GameState gameStateAfterMove = getGameStateAfterMove(moveChar, game);
+        game.setState(gameStateAfterMove);
+        return GameResponse.from(game);
+    }
+
+    private void checkCellIndex(int cellIndex, Game game) {
+        char cellValue = game.getBoard()[cellIndex];
         if (cellValue == 'X' || cellValue == 'O') {
-            throw new IllegalMoveException("Board cell " + move.getCellIndex() +
+            throw new IllegalMoveException("Board cell " + cellIndex +
                     " already contains value " + cellValue + ".");
         }
+    }
 
-        char moveMadeBy;
+    private void checkPlayer(Player player, Game game) {
+        Player xPlayer = game.getXPlayer();
+        Player oPlayer = game.getOPlayer();
         boolean validPassword;
-        boolean validMove = false;
-        GameState nextGameState;
 
-        if (game.getXPlayer().getName().equals(move.getPlayer().getName())) {
-            moveMadeBy = 'X';
-            validMove = game.getState() == X_MOVE;
-            validPassword = game.getXPlayer().getPassword().equals(move.getPlayer().getPassword());
-            nextGameState = O_MOVE;
-        } else if (game.getOPlayer().getName().equals(move.getPlayer().getName())) {
-            moveMadeBy = 'O';
-            validMove = game.getState() == O_MOVE;
-            validPassword = game.getOPlayer().getPassword().equals(move.getPlayer().getPassword());
-            nextGameState = X_MOVE;
+        if (xPlayer.getName().equals(player.getName())) {
+            validPassword = xPlayer.getPassword().equals(player.getPassword());
+        } else if (oPlayer.getName().equals(player.getName())) {
+            validPassword = oPlayer.getPassword().equals(player.getPassword());
         } else {
-            throw new IllegalMoveException("Game [" + move.getGameId() + "] cannot be played by player "
-                    + move.getPlayer().getName());
+            throw new PlayerNotFoundException("Player " + player.getName()
+                    + " not found in game [" + game.getId() + "].");
         }
 
         if (!validPassword) {
-            throw new InvalidPasswordException("Player " + move.getPlayer().getName() + " provided invalid password");
+            throw new InvalidPasswordException("Player " + player.getName() + " provided invalid password");
+        }
+    }
+
+    private void checkMove(char moveChar, Game game) {
+        boolean validMove = false;
+        if (moveChar == 'X') {
+            validMove = game.getState() == X_MOVE;
+        } else if (moveChar == 'O') {
+            validMove = game.getState() == O_MOVE;
         }
 
         switch (game.getState()) {
             case X_WON:
             case O_WON:
             case DRAW:
-                throw new IllegalMoveException("Game [" + move.getGameId() + "] already finished with " + game.getState());
+                throw new IllegalMoveException("Game [" + game.getId() + "] already finished with " + game.getState());
             case X_MOVE:
             case O_MOVE:
                 if (!validMove) {
-                    throw new IllegalMoveException("Move made by player " + moveMadeBy +
+                    throw new IllegalMoveException("Move made by player " + moveChar +
                             " but " + game.getState() + " expected.");
                 }
+            case WAITING_FOR_REGISTRATION:
         }
 
-        game.getBoard()[move.getCellIndex()] = moveMadeBy;
-        char[] board = game.getBoard();
-
-        boolean draw = true;
-        boolean won = false;
-        int i = 0;
-        do {
-            draw = (board[i] == 'X' || board[i] == 'O');
-            i++;
-        } while (draw && i < 9);
-
-        if (draw) {
-            nextGameState = DRAW;
-        } else {
-            won = (board[0] == moveMadeBy && board[1] == moveMadeBy && board[2] == moveMadeBy) ||
-                    (board[3] == moveMadeBy && board[4] == moveMadeBy && board[5] == moveMadeBy) ||
-                    (board[6] == moveMadeBy && board[7] == moveMadeBy && board[8] == moveMadeBy) ||
-
-                    (board[0] == moveMadeBy && board[3] == moveMadeBy && board[6] == moveMadeBy) ||
-                    (board[1] == moveMadeBy && board[4] == moveMadeBy && board[7] == moveMadeBy) ||
-                    (board[2] == moveMadeBy && board[5] == moveMadeBy && board[8] == moveMadeBy) ||
-
-                    (board[0] == moveMadeBy && board[4] == moveMadeBy && board[8] == moveMadeBy) ||
-                    (board[6] == moveMadeBy && board[4] == moveMadeBy && board[2] == moveMadeBy);
-
-            if (won && moveMadeBy == 'X') {
-                nextGameState = X_WON;
-            }
-
-            if (won && moveMadeBy == 'O') {
-                nextGameState = O_WON;
-            }
-        }
-
-        game.setState(nextGameState);
-        return GameResponse.from(game);
     }
 
-    private GameState getNextGameState(char[] board, char moveMadeBy) {
+    private char getMoveChar(Player player, Game game) {
+        Player xPlayer = game.getXPlayer();
+        Player oPlayer = game.getOPlayer();
+
+        char moveChar = 0;
+        if (xPlayer.getName().equals(player.getName())) {
+            moveChar = 'X';
+        } else if (oPlayer.getName().equals(player.getName())) {
+            moveChar = 'O';
+        }
+        return moveChar;
+    }
+
+    //
+    private GameState getGameStateAfterMove(char moveChar, Game game) {
+        char[] board = game.getBoard();
         GameState nextGameState = DRAW;
         boolean draw = true;
         boolean won = false;
@@ -131,22 +129,22 @@ public class GameService implements GameInterface {
         } while (draw && i < 9);
 
         if (!draw) {
-            won = (board[0] == moveMadeBy && board[1] == moveMadeBy && board[2] == moveMadeBy) ||
-                    (board[3] == moveMadeBy && board[4] == moveMadeBy && board[5] == moveMadeBy) ||
-                    (board[6] == moveMadeBy && board[7] == moveMadeBy && board[8] == moveMadeBy) ||
+            won = (board[0] == moveChar && board[1] == moveChar && board[2] == moveChar) ||
+                    (board[3] == moveChar && board[4] == moveChar && board[5] == moveChar) ||
+                    (board[6] == moveChar && board[7] == moveChar && board[8] == moveChar) ||
 
-                    (board[0] == moveMadeBy && board[3] == moveMadeBy && board[6] == moveMadeBy) ||
-                    (board[1] == moveMadeBy && board[4] == moveMadeBy && board[7] == moveMadeBy) ||
-                    (board[2] == moveMadeBy && board[5] == moveMadeBy && board[8] == moveMadeBy) ||
+                    (board[0] == moveChar && board[3] == moveChar && board[6] == moveChar) ||
+                    (board[1] == moveChar && board[4] == moveChar && board[7] == moveChar) ||
+                    (board[2] == moveChar && board[5] == moveChar && board[8] == moveChar) ||
 
-                    (board[0] == moveMadeBy && board[4] == moveMadeBy && board[8] == moveMadeBy) ||
-                    (board[6] == moveMadeBy && board[4] == moveMadeBy && board[2] == moveMadeBy);
+                    (board[0] == moveChar && board[4] == moveChar && board[8] == moveChar) ||
+                    (board[6] == moveChar && board[4] == moveChar && board[2] == moveChar);
 
-            if (moveMadeBy == 'X') {
+            if (moveChar == 'X') {
                 nextGameState = won ? X_WON : O_MOVE;
             }
 
-            if (moveMadeBy == 'O') {
+            if (moveChar == 'O') {
                 nextGameState = won ? O_WON : X_MOVE;
             }
         }
@@ -175,7 +173,6 @@ public class GameService implements GameInterface {
                 .filter(game -> game.getXPlayer().equals(player) || game.getOPlayer().equals(player))
                 .findFirst()
                 .orElseThrow(() -> new PlayerNotFoundException("Player " + player.getName() + " not found."));
-
 
         int wonAsXPlayer = Math.toIntExact(repository.games().stream()
                 .filter(game -> game.getXPlayer().equals(player))
@@ -209,7 +206,6 @@ public class GameService implements GameInterface {
                 wonAsXPlayer + wonAsOPlayer,
                 lostAsXPlayer + lostAsOPlayer,
                 drawAsXPlayer + drawAsOPlayer);
-
     }
 
 }
