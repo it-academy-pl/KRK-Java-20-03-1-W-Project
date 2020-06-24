4df1f2e9b137bfd63694bedd708ba856fdbd4d6d
artek-dz <artek.dz3007@gmail.com>
2020-06-24 12:27:29 +0200

    - tests for playAgain and getStatistics

--- src/test/java/pl/itacademy/tictactoe/api/GameControllerTest.java
+++ src/test/java/pl/itacademy/tictactoe/api/GameControllerTest.java
@@ -58,6 +58,7 @@ class GameControllerTest {
         assertThat(gameResponse.getState()).isEqualTo(GameState.WAITING_FOR_REGISTRATION);
 
         assertThat(gameRepository.getGameById(gameResponse.getGameId())).isPresent();
+
     }
 
     @Test
@@ -107,36 +108,145 @@ class GameControllerTest {
 
     @Test
     public void makeMove_properMove_returnsGameResponseWithPerformedMove() {
+        Player xPlayer = new Player("x", "pass");
+        Player oPlayer = new Player("o", "pass");
+        Game game = gameRepository.addGame(new Game(xPlayer, oPlayer, GameState.X_MOVE));
 
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json");
+        Move move = new Move(game.getId(), 0, new Player("x", "pass"));
+        HttpEntity<Move> request = new HttpEntity<>(move, headers);
+
+        ResponseEntity<GameResponse> response = restTemplate.postForEntity(uriPrefix, request, GameResponse.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        GameResponse gameResponse = response.getBody();
+        assertThat(gameResponse).isNotNull();
+        assertThat(gameResponse.getBoard()[0]).isEqualTo('X');
+        assertThat(gameResponse.getState()).isEqualTo(GameState.O_MOVE);
     }
 
     @Test
     public void makeMove_illegalMove_returnsBadRequestResponse() {
+        Player xPlayer = new Player("x", "pass");
+        Player oPlayer = new Player("o", "pass");
+        Game game = gameRepository.addGame(new Game(xPlayer, oPlayer, GameState.X_MOVE));
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json");
+
+        Move moveX = new Move(game.getId(), 0, new Player("x", "pass"));
+        HttpEntity<Move> request = new HttpEntity<>(moveX, headers);
+        ResponseEntity<GameResponse> responseX = restTemplate.postForEntity(uriPrefix, request, GameResponse.class);
+
+        Move moveO = new Move(game.getId(), 0, new Player("o", "pass"));
+        HttpEntity<Move> requestO = new HttpEntity<>(moveO, headers);
+        ResponseEntity<ErrorResponse> responseO = restTemplate.postForEntity(uriPrefix, requestO, ErrorResponse.class);
+
+        ErrorResponse error = responseO.getBody();
+        assertThat(responseO.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+        assertThat(error).isNotNull();
+        assertThat(error.getMessage()).contains("Board cell 0 already contains value X");
 
     }
 
     @Test
     public void makeMove_wrongPlayer_returnsNotFoundResponse() {
+        Player xPlayer = new Player("x", "pass");
+        Player oPlayer = new Player("o", "pass");
+        Game game = gameRepository.addGame(new Game(xPlayer, oPlayer, GameState.X_MOVE));
 
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json");
+
+        Move move = new Move(game.getId(), 0, new Player("wrongPlayer", "pass"));
+        HttpEntity<Move> request = new HttpEntity<>(move, headers);
+
+        ResponseEntity<ErrorResponse> response = restTemplate.postForEntity(uriPrefix, request, ErrorResponse.class);
+
+        ErrorResponse error = response.getBody();
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        assertThat(error).isNotNull();
+        assertThat(error.getMessage()).contains("Player wrongPlayer not found in game [1]");
     }
 
     @Test
     public void playAgain_gameNotFound_returnsNotFoundResponse() {
+        Player xPlayer = new Player("x", "pass");
+        Player oPlayer = new Player("o", "pass");
+        Game game = gameRepository.addGame(new Game(xPlayer, oPlayer, GameState.DRAW));
 
+        int gameId = game.getId() + 1;
+
+        ResponseEntity<ErrorResponse> response = restTemplate.getForEntity(uriPrefix + "/again/" + gameId, ErrorResponse.class);
+
+        ErrorResponse error = response.getBody();
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        assertThat(error).isNotNull();
+        assertThat(error.getMessage()).contains("Game [" + gameId + "] not found");
     }
 
     @Test
     public void playAgain_gameFound_returnsGameResponseWithNewGame() {
 
+        Player xPlayer = new Player("x", "pass");
+        Player oPlayer = new Player("o", "pass");
+        Game game = gameRepository.addGame(new Game(xPlayer, oPlayer, GameState.DRAW));
+
+        int gameId = game.getId();
+
+        ResponseEntity<GameResponse> response = restTemplate.getForEntity(uriPrefix + "/again/" + gameId, GameResponse.class);
+
+        GameResponse gameResponse = response.getBody();
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        System.out.println(gameResponse);
+        assertThat(gameResponse).isNotNull();
+        assertThat(gameResponse.getGameId()).isEqualTo(gameId + 1);
+        assertThat(gameResponse.getState()).isEqualTo(GameState.X_MOVE);
+
     }
 
     @Test
     public void getGameStatistic_playerNotFound_returnsNotFoundResponse() {
+        Player xPlayer = new Player("x", "pass");
+        Player oPlayer = new Player("o", "pass");
+        Game game = gameRepository.addGame(new Game(xPlayer, oPlayer, GameState.DRAW));
 
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json");
+        HttpEntity<Player> request = new HttpEntity<>(new Player("JanK", "1234"), headers);
+
+        ResponseEntity<ErrorResponse> response = restTemplate.postForEntity(uriPrefix + "/stats", request, ErrorResponse.class);
+
+        ErrorResponse error = response.getBody();
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        assertThat(error).isNotNull();
+        assertThat(error.getMessage()).contains("Player JanK not found");
     }
 
     @Test
     public void getGameStatistic_playerFound_returnsPlayerStatistic() {
+        Player xPlayer = new Player("x", "pass");
+        Player oPlayer = new Player("o", "pass");
+        Game game0 = gameRepository.addGame(new Game(xPlayer, oPlayer, GameState.DRAW));
+        Game game1 = gameRepository.addGame(new Game(xPlayer, oPlayer, GameState.X_WON));
+        Game game2 = gameRepository.addGame(new Game(xPlayer, oPlayer, GameState.X_WON));
+        Game game3 = gameRepository.addGame(new Game(xPlayer, oPlayer, GameState.O_WON));
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json");
+        HttpEntity<Player> request = new HttpEntity<>(xPlayer, headers);
+
+        ResponseEntity<GameStatistics> response = restTemplate.postForEntity(uriPrefix + "/stats", request, GameStatistics.class);
+
+        GameStatistics statistics = response.getBody();
+        assertThat(statistics).isNotNull();
+        assertThat(statistics.getDraw()).isEqualTo(1);
+        assertThat(statistics.getWon()).isEqualTo(2);
+        assertThat(statistics.getLost()).isEqualTo(1);
 
     }
 
